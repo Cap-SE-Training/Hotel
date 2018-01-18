5dd8b798a6499288a33093b8fa547d16be64bd08
INELISSE <ian.nelissen@gmail.com>
2018-01-18 14:52:02 +0100

    Made working Roomrepository and controller. Tested in Postman. Added functions for adding, deleting and listing rooms.

---
+++ src/main/java/com/capgemini/setrack/controller/RoomController.java
@@ -0,0 +1,37 @@
+package com.capgemini.setrack.controller;
+
+import com.capgemini.setrack.Model.Room;
+import com.capgemini.setrack.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/rooms/")
+public class RoomController {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Iterable<Room> getAllRooms() {
+        return this.roomRepository.rooms;
+    }
+
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public Room createRoom(@RequestBody Room room) {
+        this.roomRepository.rooms.add(room);
+
+        return room;
+    }
+
+    @RequestMapping(value = "delete/{id}", method = RequestMethod.DELETE)
+    public void deleteRoom(@PathVariable long id) {
+
+        for (Room room : this.roomRepository.rooms) {
+            if (room.getId() == id) {
+                this.roomRepository.rooms.remove(room);
+                break;
+            }
+        }
+    }
+}
