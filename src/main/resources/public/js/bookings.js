070d74f417b9599bd14ef20dde5096fd389e1a38
Mustapha Afkir <afkir01@gmail.com>
2018-01-23 16:06:55 +0100

    Created bookings landingpage

---
+++ src/main/resources/public/js/bookings.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    getBookings();
+
+    table =  $('#table').DataTable({
+        bLengthChange: false,
+        rowId: 'id',
+        columns: [
+            { "data": "roomId" },
+            { "data": "guest.name" },
+            { "data": "guest.lastName" },
+            { "data": "startDate" },
+            { "data": "endDate" },
+            { "data": "checkedIn" },
+            { "data": "checkedOut" },
+            { "data": "paid" },
+            { "data": "paymentMethod" }
+        ]
+    });
+})
+function getBookings() {
+    console.log("Getting All Bookings...");
+
+    $.ajax({
+        url:"http://localhost:8080/api/booking/",
+        type:"get",
+        success: function(bookings) {
+            console.log("This is the data: " + bookings);
+            table.clear();
+            table.rows.add(bookings);
+            table.columns.adjust().draw();
+        }
+    });
+}
