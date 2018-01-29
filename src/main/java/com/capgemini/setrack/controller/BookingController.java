50b2def48a06b7aded151c93a2a9d197b2b14765
Alexander van 't Hof <alexander.vthof@gmail.com>
2018-01-29 15:51:34 +0100

    Controller, made endpoint bookingS (plural)

--- src/main/java/com/capgemini/setrack/controller/BookingController.java
+++ src/main/java/com/capgemini/setrack/controller/BookingController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/booking/")
+@RequestMapping("/api/bookings/")
 public class BookingController {
 
     @Autowired
