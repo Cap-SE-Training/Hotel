7bbadffec886c200380a9a5f078687dd9aa5452b
SAHURK <s.vandenhurk@gmail.com>
2018-01-30 17:07:43 +0100

    validation + delete functionality phonenumber is no longer required, email address is no longer required, fistname, lastname should consist of latin characters, add cascade delete so address also gets deleted after a delete guest

--- src/main/java/com/capgemini/setrack/model/Guest.java
+++ src/main/java/com/capgemini/setrack/model/Guest.java
@@ -11,25 +11,27 @@ public class Guest extends Model{
     @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
 
+    //document toevoegen
+
     @NotNull(message = "First name is required!")
+    @Pattern(regexp="[a-zA-Z ]*",message="A name must consist of letters found in the latin alphabet")
     @Size(min=2, max=30, message="A name must be between 2 and 30 characters long!")
     private String firstName;
 
     @NotNull(message = "Last name is required!")
+    @Pattern(regexp="[a-zA-Z ]*",message="A name must consist of letters found in the latin alphabet")
     @Size(min=2, max=30, message="A name must be between 2 and 30 characters long!")
     private String lastName;
 
-    @OneToOne
+    @OneToOne(cascade=CascadeType.REMOVE)
     @JoinColumn(name="address_id")
     @NotNull(message = "Address is required!")
     private Address address;
 
-    @NotNull
     @Size(min = 5, max = 100)
     @Pattern(regexp="(?:[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",message="Email is not valid!")
     private String email;
 
-    @NotNull
     @Size(min = 10, max = 15)
     private String telephoneNumber;
 
