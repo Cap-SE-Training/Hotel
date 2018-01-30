00800c341977ec1820b03d169d53d5cbf274e53f
CORP\nopdam <niels.opdam@capgemini.com>
2018-01-30 12:50:27 +0100

    Extended table and create/update form to include address

--- src/main/resources/public/js/guests.js
+++ src/main/resources/public/js/guests.js
@@ -15,9 +15,14 @@ $(document).ready(function () {
            { "data": function( data, type, row ){
                 return data.firstName + " " + data.lastName;
            } },
-           //{ "data": "address" },
            { "data": "email" },
            { "data": "telephoneNumber" },
+           { "data": function( data, type, row ){
+               return data.address.street + " " + data.address.houseNumber;
+           } },
+           { "data": "address.postalCode" },
+           { "data": "address.city" },
+           { "data": "address.country" }
         ]
     });
 
@@ -46,7 +51,6 @@ $(document).ready(function () {
     });
     $('#guestForm').submit(function(event) {
         event.preventDefault();
-        $('#guestModal').modal('hide');
         if (edit) {
             handleEditFormSubmit();
         } else {
@@ -57,10 +61,12 @@ $(document).ready(function () {
 
 function handleCreateFormSubmit() {
     var data = getFormData();
+
     createGuest(data, function(result) {
         toastr.success('Added "' + data.firstName + ' ' + data.lastName + '" to Guests!');
         $('#guestForm').get(0).reset();
         updateTable();
+        $('#guestModal').modal('hide');
     }, handleError);
 }
 
@@ -69,9 +75,12 @@ function handleEditFormSubmit() {
     var data = getFormData();
     _.extend(guest, data);
     editGuest(guest, function(result) {
+        console.log("editing");
+        console.log(guest);
         toastr.success('Edited "' + data.firstName + ' ' + data.lastName + '"');
         $('#guestForm').get(0).reset();
         updateTable();
+        $('#guestModal').modal('hide');
         edit = false;
     }, handleError);
 }
@@ -80,14 +89,60 @@ function handleError(error) {
     console.log(error);
 };
 
-function createGuest(guest, successCallback, errorCallback) {
+function createGuest(data, successCallback, errorCallback) {
     console.log("Creating guest..")
-    ajaxJsonCall('POST', '/api/guests/create', guest, successCallback, errorCallback);
+
+    var address = {
+        street: data.street,
+        houseNumber: data.houseNumber,
+        postalCode: data.postalCode,
+        city: data.city,
+        country: data.country
+    };
+
+    var guest = {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        telephoneNumber: data.telephoneNumber
+    };
+
+    ajaxJsonCall('POST', '/api/address/create', address,
+        function(result) {
+            guest.address = result;
+            ajaxJsonCall('POST', '/api/guests/create', guest, successCallback, errorCallback);
+    }, errorCallback);
 }
 
-function editGuest(guest, successCallback, errorCallback) {
+function editGuest(data, successCallback, errorCallback) {
+
     console.log("Editing guest..")
-    ajaxJsonCall('POST', '/api/guests/edit', guest, successCallback, errorCallback);
+    var editedAddress = {
+        street: data.street,
+        houseNumber: data.houseNumber,
+        postalCode: data.postalCode,
+        city: data.city,
+        country: data.country
+    };
+
+    var editedGuest = {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        telephoneNumber: data.telephoneNumber
+    };
+
+    var address = tableHelper.getSelectedRowData().address;
+    _.extend(address, editedAddress);
+    var guest = tableHelper.getSelectedRowData();
+
+    ajaxJsonCall('POST', '/api/address/edit', address,
+        function(result) {
+            editedGuest.address = result;
+            _.extend(guest, editedGuest);
+
+            ajaxJsonCall('POST', '/api/guests/edit', guest, successCallback, errorCallback);
+    }, errorCallback);
 }
 
 function removeGuest(guest, successCallback, errorCallback) {
@@ -100,7 +155,12 @@ function getFormData() {
         firstName : $("#firstName").val(),
         lastName : $("#lastName").val(),
         email : $("#email").val(),
-        telephoneNumber : $("#telephoneNumber").val()
+        telephoneNumber : $("#telephoneNumber").val(),
+        street : $("#street").val(),
+        houseNumber : $("#houseNumber").val(),
+        postalCode : $("#postalCode").val(),
+        city : $("#city").val(),
+        country : $("#country").val()
     };
 }
 
@@ -109,6 +169,11 @@ function setFormData(guest) {
     $('#lastName').val(guest.lastName);
     $('#email').val(guest.email);
     $('#telephoneNumber').val(guest.telephoneNumber);
+    $("#street").val(guest.address.street);
+    $("#houseNumber").val(guest.address.houseNumber);
+    $("#postalCode").val(guest.address.postalCode);
+    $("#city").val(guest.address.city);
+    $("#country").val(guest.address.country);
 }
 
 function updateTable() {
