d19dba13821e970c0e679ed02ef894c634ae190f
CORP\nopdam <niels.opdam@capgemini.com>
2018-01-30 12:49:21 +0100

    Added validation to guest

--- src/main/java/com/capgemini/setrack/model/Guest.java
+++ src/main/java/com/capgemini/setrack/model/Guest.java
@@ -1,27 +1,38 @@
 package com.capgemini.setrack.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Entity
-public class Guest {
+public class Guest extends Model{
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
+
+    @NotNull(message = "First name is required!")
+    @Size(min=2, max=30, message="A name must be between 2 and 30 characters long!")
     private String firstName;
+
+    @NotNull(message = "Last name is required!")
+    @Size(min=2, max=30, message="A name must be between 2 and 30 characters long!")
     private String lastName;
 
     @OneToOne
     @JoinColumn(name="address_id")
+    @NotNull(message = "Address is required!")
     private Address address;
 
+    @NotNull
+    @Size(min = 5, max = 100)
+    @Pattern(regexp="(?:[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",message="Email is not valid!")
     private String email;
+
+    @NotNull
+    @Size(min = 10, max = 15)
     private String telephoneNumber;
 
-    @JsonIgnore
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinTable(name = "booking_guest", joinColumns = {
             @JoinColumn(name = "guest_id", referencedColumnName = "id") }, inverseJoinColumns = {
@@ -100,3 +111,4 @@ public class Guest {
         this.bookings = bookings;
     }
 }
+
